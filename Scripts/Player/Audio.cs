b68879c439d92410ad83146bdf71834d10146843
45P3R4 <51315442+45P3R4@users.noreply.github.com>
2024-04-25 06:00:36 +0300

    Shot effects

---
+++ Scripts/Player/Audio.cs
@@ -0,0 +1,11 @@
+using Godot;
+using System;
+
+public partial class Audio : AudioStreamPlayer3D
+{
+	//signal
+	public void OnFire() {
+		Play();
+
+	}
+}
--- Scripts/Player/Weapon/PlayerWeaponFire.cs
+++ Scripts/Player/Weapon/PlayerWeaponFire.cs
@@ -3,8 +3,14 @@ using System;
 
 public partial class PlayerWeaponFire : State
 {
+	[Signal]
+	public delegate void OnFireEventHandler();
+
 	[Export]
 	RayCast3D raycast;
+
+	[Export]
+	Node3D fireNode;
 
     public override void Process(float delta)
     {
@@ -13,6 +19,16 @@ public partial class PlayerWeaponFire : State
 				e.TakeDamage(10);
 			}
 		}
+		timer(0.05f);
+		
+		EmitSignal(SignalName.OnFire);
 		Fsm.TransitionTo("PlayerWeaponCooldown");
     }
+
+	private async void timer(float time) 
+	{
+		fireNode.Visible = true;
+		await ToSignal(GetTree().CreateTimer(time), SceneTreeTimer.SignalName.Timeout);
+		fireNode.Visible = false;
+	}
 }
